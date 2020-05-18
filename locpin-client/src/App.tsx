1b7465c6e3ab6afa05bed8f0b63a9cda4b1723cf
maokei <rickard.joh@gmail.com>
2020-05-18 23:15:36 +0200

    dispatch

--- locpin-client/src/App.tsx
+++ locpin-client/src/App.tsx
@@ -6,9 +6,10 @@ import { appReducer } from "./reducer";
 function App() {
 	const initialState = useContext(appContext)
 	const [ state, dispatch ] = useReducer(appReducer, initialState)
+	//dispatch("test")
 	// <Context.Provider value={{ state, dispatch }}>
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="contained" color="primary" onClick={() => dispatch("test")}>
       Hello World { state.lang }
     </Button>
 
--- locpin-client/src/reducer.tsx
+++ locpin-client/src/reducer.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { InitialStateType } from "./context"
 
-type Action = "test";
+export type Action = "test";
+
 
 export const appReducer = (state: InitialStateType, action: Action) => {
 	switch(action) {
+		case "test":
+			return {
+				...state,
+				lang: "swe"
+			}
+		//case "LOGIN_USER":
+		//	return state
 		default:
 		return state;
 	}
