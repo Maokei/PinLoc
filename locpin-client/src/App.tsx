871da1a9f771c0c64555d1d4e4654edf666ee7a4
shirley <shirley@triggybet.com>
2020-05-23 15:28:00 +0200

    Revert "add login form and style the button"
    This reverts commit 690ca02ce8228bc15b444bc8bb3cd77f896e1a8f.

--- locpin-client/src/App.tsx
+++ locpin-client/src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react'; 
-import { Container } from '@material-ui/core'
-import { Login} from './components/Login'
-import { LoginForm } from './components/LoginForm';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
 
 function App() {
   return (
-    <Container>
-      <Login/>
-      <LoginForm />
-    </Container>
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <Typography variant="h2" component="h2">
+          Roboto
+      </Typography>
+    </div>
   );
 }
 
