bdbb62ebf2755be2a282fd6c63cab91765d02507
maokei <rickard.joh@gmail.com>
2020-06-03 13:56:08 +0200

    Set test profiles, test

--- locpin-server/src/test/java/se/maokei/locpin/web/controllers/UserControllerTest.java
+++ locpin-server/src/test/java/se/maokei/locpin/web/controllers/UserControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -14,6 +15,7 @@ import se.maokei.locpin.repository.UserRepository;
 
 import javax.persistence.EntityManager;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @EnableWebMvc
