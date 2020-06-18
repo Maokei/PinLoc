667907dbabe43acd7915c74e9b0cbd7df7565cda
maokei <rickard.joh@gmail.com>
2020-06-19 00:52:00 +0200

    map rendering

--- locpin-client/src/components/Map.tsx
+++ locpin-client/src/components/Map.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import '../../node_modules/leaflet/dist/leaflet.css'
 
 const LeafletMap = () => {
-    type test = {
+    /*test = {
         lat: 51.505,
         lng: -0.09,
         zoom: 13,
-      }
+      }*/
     const position = [ 51.505, -0.09 ]
     return (
-        <Map center={[45.4, -75.7]} zoom={12}>
+      <Map center={[50, 10]} zoom={12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
       />
     </Map>
     );
