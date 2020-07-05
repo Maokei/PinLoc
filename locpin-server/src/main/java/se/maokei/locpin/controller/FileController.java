1bdcc5b3bf3a288e2cda383f807c3acf5f54cd5c
maokei <rickard.joh@gmail.com>
2020-07-05 23:52:16 +0200

    FileUpload controller

---
+++ locpin-server/src/main/java/se/maokei/locpin/controller/FileController.java
@@ -0,0 +1,11 @@
+package se.maokei.locpin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
+
+}
