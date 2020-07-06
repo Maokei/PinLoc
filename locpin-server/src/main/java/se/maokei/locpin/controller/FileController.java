52a624becf4b55ebf602a2e6d6589f19f68a63a3
maokei <rickard.joh@gmail.com>
2020-07-06 23:40:44 +0200

    Photo add method

--- locpin-server/src/main/java/se/maokei/locpin/controller/FileController.java
+++ locpin-server/src/main/java/se/maokei/locpin/controller/FileController.java
@@ -2,10 +2,18 @@ package se.maokei.locpin.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class FileController {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
 
+    @PostMapping("/photos/add")
+    public void addPhoto(@RequestParam("image") MultipartFile image) throws IOException {
+    }
 }
