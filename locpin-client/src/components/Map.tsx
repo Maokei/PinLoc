51570613c21532e1348b85d029eb6812a83457a3
maokei <rickard.joh@gmail.com>
2020-07-12 18:48:00 +0200

    Moving to CDN for leaflet

--- locpin-client/src/components/Map.tsx
+++ locpin-client/src/components/Map.tsx
@@ -11,6 +11,13 @@ const LeafletMap = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
+        <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup.
+                        <br />
+                        Easily customizable.
+                    </Popup>
+                </Marker>
       </Map>
     </div>
     );
