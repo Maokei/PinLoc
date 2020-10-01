83a2d0c8fbd4188ee50fd7aa05999f5903d73df4
shirley <shirleywang527@gmail.com>
2020-10-01 12:36:50 +0200

    test leaflet map marker

--- locpin-client/src/components/Map.tsx
+++ locpin-client/src/components/Map.tsx
@@ -1,26 +1,29 @@
-import React from "react";
+import * as React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import './map.css';
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+// import "./map.css";
 
-const LeafletMap = () => {
-    const position = [ 51.505, -0.09 ]
-    return (
-    <div className="map-wrapper">
-      <Map center={[50, 10]} zoom={12}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup.
-                        <br />
-                        Easily customizable.
-                    </Popup>
-                </Marker>
-      </Map>
-    </div>
-    );
+L.Icon.Default.mergeOptions({
+	iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+	iconUrl: require("leaflet/dist/images/marker-icon.png"),
+	shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+const LeafletMap = (props: { center: any; zoom: any }) => {
+	return (
+		<Map center={props.center} zoom={props.zoom} style={{ height: "100vh" }}>
+			<TileLayer
+				attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+			/>
+			<Marker position={props.center}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker>
+		</Map>
+	);
 };
 
 export default LeafletMap;
