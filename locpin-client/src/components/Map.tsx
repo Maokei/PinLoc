6b76ac222bb3c3193922c1de31fc6bf4eb3c3c23
shirley <shirleywang527@gmail.com>
2020-10-03 14:39:11 +0200

    fix style of map height

--- locpin-client/src/components/Map.tsx
+++ locpin-client/src/components/Map.tsx
@@ -4,6 +4,7 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "./map.css";
 
 let DefaultIcon = L.icon({
 	iconUrl: icon,
