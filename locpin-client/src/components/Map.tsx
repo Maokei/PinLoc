ad2991ff3abf27b1863d1ec524652f5742d371f7
shirley <shirleywang527@gmail.com>
2020-10-03 14:25:59 +0200

    enable pin and marker to show up on map

--- locpin-client/src/components/Map.tsx
+++ locpin-client/src/components/Map.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-// import "./map.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-	iconUrl: require("leaflet/dist/images/marker-icon.png"),
-	shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+let DefaultIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow,
 });
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const LeafletMap = (props: { center: any; zoom: any }) => {
 	return (
