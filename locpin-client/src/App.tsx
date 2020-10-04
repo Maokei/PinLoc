c62fd38fe84d84649314886399d24e67ddc62b64
shirley <shirleywang527@gmail.com>
2020-10-04 13:08:10 +0200

    clean up

--- locpin-client/src/App.tsx
+++ locpin-client/src/App.tsx
@@ -1,30 +1,11 @@
-import React, { useContext, useReducer } from "react";
-import Button from "@material-ui/core/Button";
-import { AppContext } from "./context";
-import { ActionType, AppReducer } from "./reducer";
+import * as React from "react";
+import Buttons from "./components/Buttons";
 import Map from "./components/Map";
 
 function App() {
-	const initialState = useContext(AppContext);
-	const [state, dispatch] = useReducer(AppReducer, initialState);
 	return (
 		<div className="app">
-			<Button
-				variant="contained"
-				color="primary"
-				onClick={() =>
-					dispatch({ type: ActionType.USER_LOGIN, payload: "swe" })
-				}>
-				Hello World {state.lang}
-			</Button>
-			<Button
-				variant="contained"
-				color="primary"
-				onClick={() =>
-					dispatch({ type: ActionType.USER_LOGOUT, payload: "eng" })
-				}>
-				Reset
-			</Button>
+			<Buttons />
 			<Map />
 		</div>
 	);
